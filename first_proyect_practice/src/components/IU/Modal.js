a5ab536f61c0c13f21108b22ea038fc6bd9ee46a
candrade <leodark17@hotmail.com>
2022-08-18 20:44:05 -0500

    Se modifico el Modal, con el uso de createPortal

--- first_proyect_practice/src/components/IU/Modal.js
+++ first_proyect_practice/src/components/IU/Modal.js
@@ -1,18 +1,18 @@
 import Button from './Button';
+import ReactDOM from 'react-dom';
 import style from './Modal.module.css';
 
 export default function Alert(props) {
 
-  let showModal, animationClose;
-  if (props.closeModal != null) {
-    showModal = `${style.show} ${style.fade}`;
-  } else {
-    animationClose = `${style["modal-animation-close"]}`;
+  const Backdrop = (props) => {
+    return (
+      <div className={`${style.modal} ${showModal}`} role="dialog" onClick={props.closeModal}></div>
+    );
   }
 
-  return (
-    <div className={`${style.modal} ${showModal}`} role="dialog" onClick={props.closeModal}>
-      <div className={`${style["modal-dialog"]} ${animationClose}`}>
+  const ModalOverlay = (props) => {
+    return (
+      <div className={`${style["modal-dialog"]} ${animationClose}`} onClick={props.closeModal}>
         <div className={style["modal-content"]}>
           <div className={style["modal-header"]}>
             <h5 className={style["modal-title"]}>{props.messageTitle}</h5>
@@ -25,6 +25,27 @@ export default function Alert(props) {
           </div>
         </div>
       </div>
-    </div>
+    );
+  }
+
+  let showModal, animationClose;
+  if (props.closeModal != null) {
+    showModal = `${style.show} ${style.fade}`;
+  } else {
+    animationClose = `${style["modal-animation-close"]}`;
+  }
+
+  /**
+   * * createPortal permite agregar elementos secundarios en un nodo DOM que existe fuera de la jerarquia DOM del 
+   * * componente principal, como en este caso que se crea un div fuera del div root
+   * * El primer parametro que recibe es cualquier elemento secundario de React renderizable, como un elemento, cadena o fragmento
+   * * El segundo parametro que recibe es el elemento DOM donde se mostrara 
+   */
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop closeModal={props.closeModal} />, document.getElementById('backdrop-root'))}
+      {ReactDOM.createPortal(<ModalOverlay messageTitle={props.messageTitle} messageBody={props.messageBody} closeModal={props.closeModal} />,
+        document.getElementById('overlay-root'))}
+    </>
   );
 }

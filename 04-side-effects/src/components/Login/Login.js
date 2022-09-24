3abaffe9a21bcff1b3efbd6c3f787d6f1c6fbf42
candrade <leodark17@hotmail.com>
2022-09-24 00:28:02 -0500

    se vio el tema de side-effects de react hooks useEffect

---
+++ 04-side-effects/src/components/Login/Login.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  /*
+  * Se valida que los datos ingresados por el usuario, sean correctos, para proceder a iniciar sesion
+  * se usa useEffect, para evitar el copy and paste de la condicion setFormIsValid
+  * Como enteredEmail y enteredPassword, estaran cambiando constantemente, se asigna como dependencias
+  */
+  useEffect(() => {
+
+    const identifier = setTimeout(() => {
+      console.log('se creo el efecto secundario');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    /*
+    * Esta seccion nos permite que no se realizen varias peticiones cada vez que el usuario teecle algo
+    * permitiendo reiniciar el temporizador y ejecutarlo solo hasta que haya pasado determinado tiempo
+    */
+    return () => {
+      console.log('Se ha borrado el efecto secundario');
+      clearTimeout(identifier);
+    }
+
+
+  }, [enteredEmail, enteredPassword]);
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const validateEmailHandler = () => {
+    setEmailIsValid(enteredEmail.includes('@'));
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+            }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''
+            }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
